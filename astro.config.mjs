67a0d6f5a5581b0e07efe24bedac33b697361c21
jeshol <jesholmlund@gmail.com>
2023-09-13 17:27:48 +0200

    adapter to object

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: "nodejs()",
+  adapter: nodejs(),
   integrations: [tailwind()],
 });
