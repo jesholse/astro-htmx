6676254b2d74f074c9f14bc7053d0828f41f0596
jesper <jesholmlund@gmail.com>
2023-11-22 23:09:03 +0100

    adapter, clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import nodejs from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: nodejs({
-    mode: "standalone",
-  }),
-  integrations: [tailwind()],
+  adapter: netlify(),
+  integrations: [tailwind()]
 });
